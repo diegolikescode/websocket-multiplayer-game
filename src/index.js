a52e563d451c47a2640c93b704fb7142b3121c4c
diegolikescode <diegoprestes.git@hotmail.com>
2023-09-02 16:06:57 -0300

    create and join event

--- src/index.js
+++ src/index.js
@@ -1,24 +1,71 @@
-import { randomUUID} from 'crypto'
-const server = require('http').createServer()
-const WebsocketServer = require('websocket').server
-
 import http from 'http'
+import {server}from 'websocket'
+import { randomUUID} from 'crypto'
 
-server.listen(6969, () => console.log("listening on 6969"))
+const httpServer = http.createServer()
+httpServer.listen(6969, () => console.log("listening on 6969"))
 
-const wsServer = new WebsocketServer({
-    'httpServer': server
+const wsServer = new server({
+    'httpServer': httpServer
 })
 
 const clients = {}
+const games = {}
 
 wsServer.on('request', req => {
-    const conn = req.accept(null, request.origin)
+    const conn = req.accept(null, req.origin)
     conn.on('open', () => console.log('open'))
     conn.on('close', () => console.log('closed'))
+
     conn.on('message', msg => {
-        // (msg, conn)
-        console.log(JSON.parse(msg.utf8Data))
+        const result = JSON.parse(msg.utf8Data)
+
+        if (result.method === 'create') {
+            const {clientId} = result
+            const gameId = randomUUID()
+
+            const player = {clientId, color: 'red'}
+
+            games[gameId] = {
+                id: gameId,
+                balls: 20,
+                clients: [player]
+            }
+
+            const payload = {
+                method: 'create',
+                game: games[gameId]
+            }
+
+            const con = clients[clientId].connection
+            console.log(games)
+            con.send(JSON.stringify(payload))
+        }
+
+        if (result.method === 'join') {
+            const {clientId, gameId} = result
+
+            const game = games[gameId]
+            if (game.clients.length >= 3) {
+                const con = clients[clientId].connection
+                con.send(JSON.stringify({error: 'max players reached'}))
+                return
+            }
+            const color = {'0': 'red', '1': 'green', '2': 'blue'}[game.clients.length]
+            game.clients.push({
+                clientId,
+                color
+            })
+
+            const payload = {
+                method: 'join',
+                game
+            }
+
+            game.clients.forEach(cli => {
+                clients[cli.clientId].connection.send(JSON.stringify(payload))
+            })
+        }
     })
 
     handleRequest(conn)
@@ -35,5 +82,5 @@ function handleRequest(connection) {
         clientId: newClientId,
     }
 
-    conn.send(JSON.stringify(payload))
+    connection.send(JSON.stringify(payload))
 }
